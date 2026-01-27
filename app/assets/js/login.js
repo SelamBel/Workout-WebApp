50750ae8484c3b7b92203c54dd550925ee66110a
Selam Bel <116879445+SelamBel@users.noreply.github.com>
2026-01-27 11:16:03 +0100

    Refactor login.js and update number input styles
    Refactored login.js to modularize event binding and image rotation logic, improving readability and maintainability. Updated style.css to remove number input spinners for a cleaner UI.

--- app/assets/js/login.js
+++ app/assets/js/login.js
@@ -1,40 +1,43 @@
-setInterval(changePic, 5000);
-$("#rotationImage").on("click", function () {
-    changePic();
+$(document).ready(function () {
+    init();
 });
 
-function changePic() {
-    const currentSrc = $("#rotationImage").attr("src");
-    const images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
-    const current = currentSrc.split("/").pop();
-    const idx = images.indexOf(current);
-    const nextIdx = (idx + 1) % images.length;
-    $("#rotationImage").attr("src", "app/assets/images/" + images[nextIdx]);
+function init() {
+    startImageRotation();
+    bindEvents();
 }
 
+function bindEvents() {
+    $("#rotationImage").on("click", changePic);
+    $("#loginBtn").on("click", toggleLogin);
+    $("#registerBtn").on("click", toggleRegister);
+    $("#submitLogin").on("click", submitLogin);
+}
 
-$("#loginBtn").on("click", function () {
-    if ($("#loginCard").hasClass("hidden")) {
-        $("#loginCard").removeClass("hidden");
-    } else {
-        $("#loginCard").addClass("hidden");
-    }
+const images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
+const imagePath = "app/assets/images/";
 
-    if (!$("#registerCard").hasClass("hidden")) {
-        $("#registerCard").addClass("hidden");
-    }
-});
+function startImageRotation() {
+    setInterval(changePic, 5000);
+}
 
-$("#registerBtn").on("click", function () {
-    if ($("#registerCard").hasClass("hidden")) {
-        $("#registerCard").removeClass("hidden");
-    } else {
-        $("#registerCard").addClass("hidden");
-    }
-    if (!$("#loginCard").hasClass("hidden")) {
-        $("#loginCard").addClass("hidden");
-    }
-});
+function changePic() {
+    const $img = $("#rotationImage");
+    const currentSrc = $img.attr("src");
+    const currentImage = currentSrc.split("/").pop();
+    const currentIndex = images.indexOf(currentImage);
+    const nextIndex = (currentIndex + 1) % images.length;
 
+    $img.attr("src", imagePath + images[nextIndex]);
+}
 
+function toggleLogin() {
+    $("#loginCard").toggleClass("hidden");
+    $("#registerCard").addClass("hidden");
+}
+
+function toggleRegister() {
+    $("#registerCard").toggleClass("hidden");
+    $("#loginCard").addClass("hidden");
+}
 
